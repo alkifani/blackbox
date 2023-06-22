358a60f6197c1a20461d635dec8852fc048e80db
Aidil Kamal <aidilk.b20@mhs.unsyiah.ac.id>
2023-06-22 22:35:18 +0700

    V.3
    - ganti front end login
    - simpan email user
    - tampilkan nama user, jumlah trip, dan jumlah pelanggaran

--- lib/autentikasi/components/login.dart
+++ lib/autentikasi/components/login.dart
@@ -25,75 +25,106 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 30.0,
-              ),
-              Container(
-                width: 215,
-                height: 215,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/logo.png'),
-                  ),
+      backgroundColor: Color.fromRGBO(108, 108, 108, 1.0),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 75,
                 ),
-              ),
-              const SizedBox(
-                height: 40.0,
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width / 1.5,
-                child: TextField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(hintText: 'Email'),
-                ),
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width / 1.5,
-                child: TextField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    hintText: 'Password',
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  final message = await AuthService().login(
-                    email: _emailController.text,
-                    password: _passwordController.text,
-                  );
-                  if (message!.contains('Success')) {
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            Home(email: _emailController.text),
-                      ),
-                    );
-                  }
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(message),
+                Container(
+                  width: 165,
+                  height: 165,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/logo.png'),
                     ),
-                  );
-                },
-                child: const Text('Login'),
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-            ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 175.0,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width / 1.5,
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      hintText: 'EMAIL',
+                      hintStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                      fillColor: Color.fromRGBO(217, 217, 217, 1),
+                      filled: true,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 1.5,
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                      hintText: 'KATA SANDI',
+                      fillColor: Color.fromRGBO(217, 217, 217, 1),
+                      filled: true,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 36.0,
+                ),
+                SizedBox(
+                  height: 55,
+                  width: MediaQuery.of(context).size.width / 2.25,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      final message = await AuthService().login(
+                        email: _emailController.text,
+                        password: _passwordController.text,
+                      );
+                      if (message!.contains('Success')) {
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                Home(email: _emailController.text),
+                          ),
+                        );
+                      }
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(message),
+                        ),
+                      );
+                    },
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15), // Adjust the corner radius as desired
+                        ),
+                      ),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        Color.fromRGBO(23, 46, 130, 0.6), // Red color (RGB: 255, 0, 0)
+                      ),
+                    ),
+                    child: const Text('MASUK', style: TextStyle(fontWeight: FontWeight.w800, fontSize: 17),),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/menu_home/home_page.dart
+++ lib/menu_home/home_page.dart
@@ -1,7 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   static const routeName = "/HomePage";
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Future<DocumentSnapshot> _documentFuture;
+  String? emailValue;
+  int jumlahTrip = 0; // Variable to store the document count
+  int totalPelanggaran = 0; // Variable to store the sum of 'pelanggaran' field
+
+  @override
+  void initState() {
+    super.initState();
+    final user = FirebaseAuth.instance.currentUser;
+    _documentFuture = getDocument(user!.uid);
+  }
+
+  Future<DocumentSnapshot> getDocument(String userId) async {
+    final snapshot = await FirebaseFirestore.instance
+        .collection('Nama')
+        .doc(userId)
+        .get();
+    return snapshot;
+  }
+
+  Future<int> getCountOfDocuments() async {
+    final querySnapshot = await FirebaseFirestore.instance
+        .collection('DataPerjalanan')
+        .where('email', isEqualTo: emailValue)
+        .get();
+    return querySnapshot.docs.length;
+  }
+
+  Future<int> getSumOfPelanggaran() async {
+    final querySnapshot = await FirebaseFirestore.instance
+        .collection('DataPerjalanan')
+        .where('email', isEqualTo: emailValue)
+        .get();
+
+    int count = 0;
+
+    for (final doc in querySnapshot.docs) {
+      final pelanggaran = doc['pelanggaran'] as Map<dynamic, dynamic>?;
+
+      if (pelanggaran != null) {
+        count += pelanggaran.length;
+      }
+    }
+
+    return count;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +64,54 @@ class HomePage extends StatelessWidget {
         title: const Text('Home Page'),
       ),
       body: Center(
-        child: Text('Welcome to the Home Page!'),
+        child: FutureBuilder<DocumentSnapshot>(
+          future: _documentFuture,
+          builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else if (snapshot.hasData) {
+              final document = snapshot.data!;
+              final namaValue = document['nama'] as String?;
+              emailValue = document['email'] as String?; // Assign value to emailValue
+
+              if (emailValue != null) {
+                getCountOfDocuments().then((count) {
+                  setState(() {
+                    jumlahTrip = count;
+                  });
+                });
+
+                getSumOfPelanggaran().then((sum) {
+                  setState(() {
+                    totalPelanggaran = sum;
+                  });
+                });
+              }
+
+              return Column(
+                children: [
+                  if (namaValue != null)
+                    Text('Welcome to the Home Page, $namaValue!'),
+                  if (emailValue != null)
+                    Text('Your email is: $emailValue'),
+                  Text('Jumlah Trip: $jumlahTrip'),
+                  Text('Total Pelanggaran: $totalPelanggaran'),
+                  if (snapshot.connectionState == ConnectionState.done)
+                    Text('Sum of Pelanggaran: $totalPelanggaran'),
+                ],
+              );
+            } else {
+              return Text('Welcome to the Home Page!');
+            }
+          },
+        ),
       ),
     );
   }
 }
+
+
+
+
